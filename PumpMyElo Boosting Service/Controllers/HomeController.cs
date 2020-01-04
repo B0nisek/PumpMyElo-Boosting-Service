748cdbea4b52b5d1fac72ed366c34f44c04d5bd4
bonisek <crypto.boni@protonmail.com>
2020-01-04 19:50:32 +0100

    init

---
+++ PumpMyElo Boosting Service/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PumpMyElo_Boosting_Service.Models;
+
+namespace PumpMyElo_Boosting_Service.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+        }
+    }
+}
